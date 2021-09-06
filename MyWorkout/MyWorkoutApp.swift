fdc0d0c8744e5be479b96401b9cf0d4e7daede08
Chanoknun Choosaksilp <derpy33074@gmail.com>
2021-09-06 13:38:17 +0700

    Initial Commit

---
+++ MyWorkout/MyWorkoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyWorkoutApp.swift
+//  MyWorkout
+//
+//  Created by Chanoknun Choosaksilp on 6/9/2564 BE.
+//
+
+import SwiftUI
+
+@main
+struct MyWorkoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
